96b36ae70723e3479d5bc059bc0d09b47f154c09
Khalil Mubarak <khalil.f.mubarak@gmail.com>
2022-12-17 21:32:13 -0500

    Dynamic breed name query implemented

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,8 +1,12 @@
 const request = require('request');
 const arguments = process.argv.slice(2);
+const breed = arguments[0];
 
-request('https://api.thecatapi.com/v1/breeds/search?q=Siberian', (error, response, body) => {
+request('https://api.thecatapi.com/v1/breeds/search?q=' + breed, (error, response, body) => {
   
+if (error){
+  throw error;
+}
   const data = JSON.parse(body);
   console.log(body);
   console.log(typeof body);
