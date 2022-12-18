0ba0245c4c230afd01b479c3e2e40870b620276f
Khalil Mubarak <khalil.f.mubarak@gmail.com>
2022-12-17 23:32:05 -0500

    Fixed error in variable name, formatting

--- breedFetcher.js
+++ breedFetcher.js
@@ -2,7 +2,7 @@ const request = require('request');
 
 const fetchBreedDescription = function(breedName, callback) {
 
-  request('https://api.thecatapi.com/v1/breeds/search?q=' + breed, (error, response, body) => {
+  request('https://api.thecatapi.com/v1/breeds/search?q=' + breedName, (error, response, body) => {
 
     if (error) {
       callback(error, null);
