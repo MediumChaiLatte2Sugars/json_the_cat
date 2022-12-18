d615bb79eaa45e68fb683da13c8acba665de8864
Khalil Mubarak <khalil.f.mubarak@gmail.com>
2022-12-17 21:41:09 -0500

    breedFetcher completed to spec

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,15 +1,22 @@
 const request = require('request');
-const arguments = process.argv.slice(2);
-const breed = arguments[0];
+const args = process.argv.slice(2);
+const breed = args[0];
 
 request('https://api.thecatapi.com/v1/breeds/search?q=' + breed, (error, response, body) => {
-  
-if (error){
-  throw error;
-}
-  const data = JSON.parse(body);
-  console.log(body);
-  console.log(typeof body);
 
-  console.log(data[0]["description"]);
+  if (error) {
+    throw error;
+  }
+
+  console.log('Status: ', response.statusCode);
+
+  const data = JSON.parse(body);
+
+  // Check if breed data recieved
+  if (data.length === 0) {
+    console.log("Breed not found!");
+  } else {
+    console.log(data[0]["description"]);
+  }
+
 });
